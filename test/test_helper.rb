b252cfcc810059686570ee037203100ea15db495
Alex Musayev <alex.musayev@gmail.com>
2020-08-29 12:51:04 +0300

    Disable Minitest reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,4 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "callee"
 
 require "minitest/autorun"
-require "minitest/reporters"
 require "pry"
-
-Minitest::Reporters.use!([Minitest::Reporters::DefaultReporter.new(color: true)])
